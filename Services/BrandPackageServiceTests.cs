7a2dd4e7de1ff8400ad4fd26ac78b3f551ff183f
karolbronowski <karol.bronowski@gmail.com>
2019-04-15 15:29:30 +0200

    note added

--- Services/BrandPackageServiceTests.cs
+++ Services/BrandPackageServiceTests.cs
@@ -14,6 +14,7 @@ using System.Linq;
 
 namespace hostingRatingWebApi.Tests.Services
 {
+    //Tests for BrandPackageService
     public class BrandPackageServiceTests
     {
         [Fact]
--- Services/BrandServiceTests.cs
+++ Services/BrandServiceTests.cs
@@ -14,6 +14,7 @@ using Xunit;
 
 namespace hostingRatingWebApi.Tests.Services
 {
+    //Tests for BrandService 
     public class BrandServiceTests
     {
         [Fact]
